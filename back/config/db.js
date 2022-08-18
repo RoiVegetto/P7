965776a5fa988bb9aa313a6f52947d21308c26e0
Benjamin Corroyez <benjamincorroyez@hotmail.fr>
2022-08-18 22:38:04 +0200

    erreur : token B in JSON at position 17, qui me bloque la création d'un utilisateur dans le back

--- back/config/db.js
+++ back/config/db.js
@@ -1,13 +1,7 @@
 const mongoose = require("mongoose");
 
-mongoose
-    .connect('mongodb+srv://" + process.env.DB_USER_PASS + "@cluster0.9l0qgmw.mongodb.net/P7',
-        {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            useCreateIndex: true,
-            useFindAndModify: false
-        }
-    )
-    .then(() => console.log('Connected to MongoDB'))
-    .catch((err) => console.log('Failed to connect to MongoDB', err));
+mongoose.connect('mongodb+srv://' + process.env.DB_USER_PASS + '@cluster0.9l0qgmw.mongodb.net/?retryWrites=true&w=majority',
+    { useNewUrlParser: true,
+      useUnifiedTopology: true })
+.then(() => console.log('Connexion à MongoDB réussie !'))
+.catch((err) => console.log('Connexion à MongoDB échouée !', err));
--- back/server.js
+++ back/server.js
@@ -1,16 +1,19 @@
+const bodyParser = require('body-parser');
 const express = require('express');
+const userRoutes = require('./routes/user.routes');
 require('dotenv').config({path: './config/.env'});
 require('./config/db');
 const app = express();
 
-
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 
 //routes
-//app.use('/api/user', userRoutes);
+app.use('/api/user', userRoutes);
 
 
 // server
-app.listen(5000, () => {
+app.listen(process.env.PORT, () => {
     console.log(`listening on port ${process.env.PORT}`);
 })
