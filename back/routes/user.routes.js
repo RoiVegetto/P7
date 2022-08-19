f24f63c12d57b3040392df694452d66a6f720348
Benjamin Corroyez <benjamincorroyez@hotmail.fr>
2022-08-19 16:05:07 +0200

    Login, jsonwebtoken, ajouté

--- back/routes/user.routes.js
+++ back/routes/user.routes.js
@@ -4,9 +4,13 @@ const userController = require('../controllers/user.controller');
 
 // auth
 router.post("/register", authController.signUp);
+router.post('/login', authController.signIn);
+router.get('/logout', authController.logout);
 
 //user DB 
 router.get('/', userController.getAllUsers);
 router.get('/:id', userController.userInfo);
+router.put('/:id', userController.updateUser);
+router.delete('/:id', userController.deleteUser);
 
 module.exports = router;
